810af7ddf7a8a61597fe184a88bc05fcb6ffe1d2
Christopher Dearie <cdearie@gmail.com>
2021-09-09 13:00:43 -0700

    Super Sweet Feature

--- PSHTML-AD.ps1
+++ PSHTML-AD.ps1
@@ -57,7 +57,7 @@ param (
 	#Logo that will be on the right side, UNC or URL
 
 	[Parameter(ValueFromPipeline = $true, HelpMessage = "Enter URL or UNC path for Side Logo")]
-	[String]$RightLogo = "https://www.psmpartners.com/wp-content/uploads/2017/10/porcaro-stolarek-mete.png",
+	[String]$RightLogo = "https://images.app.goo.gl/guWwgyWLAwqHRaoZ7",
 	#Title of generated report
 
 	[Parameter(ValueFromPipeline = $true, HelpMessage = "Enter desired title for report")]
